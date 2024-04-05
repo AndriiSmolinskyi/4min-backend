ed084096b9f18302e5292c4b01c90341802cbef4
AndriiSmolinskyi <awful.braid@gmail.com>
2024-04-05 15:32:42 +0300

    1

--- index.js
+++ index.js
@@ -17,4 +17,8 @@ app.use(express.json())
 app.use('/api', userRouter)
 app.use('/api', postRouter)
 
+app.get('/', (req, res) => {
+    res.send('Welcome to my API!');
+});
+
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
