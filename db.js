33b7f8bc8b512e4464e826a8dd36bfbc5490b5bd
AndriiSmolinskyi <awful.braid@gmail.com>
2024-04-05 15:36:41 +0300

    1

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL ,
+    user ="default",
+    host ="ep-silent-bonus-a2xob3jn-pooler.eu-central-1.aws.neon.tech",
+    password="m0BLGHwXCj2S",
+    database="verceldb",
 })
 
 module.exports = pool
