13d6c8a26f675cbccbcbbf344da31198cb41ee8a
AndriiSmolinskyi <awful.braid@gmail.com>
2024-04-09 13:35:01 +0300

    1

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const Pool = require('pg').Pool
+const POSTGRES_URL='postgres://default:m0BLGHwXCj2S@ep-silent-bonus-a2xob3jn-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require'
 
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL ,
